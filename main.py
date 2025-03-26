d106cce8bbcc5da89b7693e75bb28530407ed932
Alexander Christian <alexanderechristian@gmail.com>
2025-03-26 13:58:20 -0400

    Automated login to SNOW

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-import logging
 import sys
-import os
+import json
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.edge.service import Service
 from selenium.webdriver.edge.options import Options
 from webdriver_manager.microsoft import EdgeChromiumDriverManager
+import keyring
 
 
 def setup_webdriver():
@@ -24,5 +25,13 @@ if __name__ == "__main__":
     driver.get(
         "https://nysifprod.service-now.com/now/nav/ui/classic/params/target/task_list.do%3Fsysparm_query%3Dactive%253Dtrue%255Eassignment_group%253D2d636adcdb4957006a2c9837db96193e%255Estate!%253D6%255EnumberNOT%2520LIKERITM%26sysparm_first_row%3D1%26sysparm_view%3D"
     )
+    SNOW_USERNAME = "achristi@nysif.com"
+    textbox_username = driver.find_element(By.ID, "userNameInput")
+    textbox_password = driver.find_element(By.ID, "passwordInput")
+    textbox_username.send_keys(SNOW_USERNAME)
+    textbox_password.send_keys(
+        keyring.get_password(service_name="ServiceNow", username=SNOW_USERNAME)
+    )
+    textbox_password.submit()
     input("Press enter to continue...")
     sys.exit()

d4dd1b71b4c916180c8034638e10d7548d38c9b1
Alexander Christian <alexanderechristian@gmail.com>
2025-03-27 13:56:00 -0400

    Removed commented commands. Added explicit return types. Added function to test if page is loaded and added wait command to wait for page load. Removed time.sleep and added NoSuchShadowRootException handling.

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+import os
 import time
+from typing import List
+from dataclasses import dataclass
+import keyring
 from selenium import webdriver
 from selenium.webdriver.edge.webdriver import WebDriver as EdgeWebDriver
 from selenium.webdriver.common.by import By
@@ -7,12 +11,10 @@ from selenium.webdriver.edge.service import Service
 from selenium.webdriver.edge.options import Options
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import NoSuchShadowRootException, TimeoutException
 from webdriver_manager.microsoft import EdgeChromiumDriverManager
-from typing import List
-from dataclasses import dataclass
-import keyring
 
-TIMEOUT = 20
+TIMEOUT = 10
 
 
 @dataclass
@@ -24,20 +26,22 @@ class Task:
     opened: str
 
 
-def setup_webdriver():
+def setup_webdriver() -> None:
+    # os.environ["ELECTRON_ENABLE_LOGGING"] = "true"
+    # os.environ["NO_REMOTE_DEBUGGING"] = "1"
+    # os.environ["NO_SANDBOX"] = "1"
+    # os.environ["NO_USER_DATA_DIR"] = "1"
     edge_options = Options()
     edge_options.use_chromium = True
     edge_options.add_argument("--no-sandbox")
-    edge_options.add_argument("--headless")
+    # edge_options.add_argument("--headless")
     edge_options.add_experimental_option("excludeSwitches", ["enable-logging"])
-    # edge_options.add_experimental_option("detach", True)
-
     driver_path = EdgeChromiumDriverManager().install()
     service = Service(driver_path)
     return webdriver.Edge(service=service, options=edge_options)
 
 
-def login(web_driver: EdgeWebDriver):
+def login(web_driver: EdgeWebDriver) -> None:
     username = "achristi@nysif.com"
     textbox_username = web_driver.find_element(By.ID, "userNameInput")
     textbox_password = web_driver.find_element(By.ID, "passwordInput")
@@ -46,6 +50,20 @@ def login(web_driver: EdgeWebDriver):
         keyring.get_password(service_name="ServiceNow", username=username)
     )
     textbox_password.submit()
+
+
+def page_loaded(web_driver: EdgeWebDriver, expected_url: str) -> bool:
+    try:
+        WebDriverWait(web_driver, TIMEOUT).until(EC.url_to_be(expected_url))
+        WebDriverWait(web_driver, TIMEOUT).until(
+            lambda driver: driver.execute_script("return document.readyState")
+            == "complete"
+        )
+    except TimeoutException as e:
+        print(f"Timed out while waiting for the page to load. Error: {e}")
+        return False
+    else:
+        return True
 
 
 def dump_elements(elements: List[WebElement]):
@@ -72,32 +90,33 @@ def parse_table(web_driver: EdgeWebDriver):
 
 
 if __name__ == "__main__":
+    url = "https://nysifprod.service-now.com/now/nav/ui/classic/params/target/task_list.do%3Fsysparm_nostack%3Dtrue%26sysparm_query%3Dactive%253Dtrue%255Eassignment_group%253D2d636adcdb4957006a2c9837db96193e%255Estate!%253D6%255EnumberNOT%2520LIKERITM%26sysparm_first_row%3D1%26sysparm_view%3D"
     driver = setup_webdriver()
-    driver.get(
-        "https://nysifprod.service-now.com/now/nav/ui/classic/params/target/task_list.do%3Fsysparm_nostack%3Dtrue%26sysparm_query%3Dactive%253Dtrue%255Eassignment_group%253D2d636adcdb4957006a2c9837db96193e%255Estate!%253D6%255EnumberNOT%2520LIKERITM%26sysparm_first_row%3D1%26sysparm_view%3D"
-    )
+    driver.get(url)
     login(driver)
-    time.sleep(5)
+    WebDriverWait(driver, TIMEOUT).until(lambda driver: page_loaded(driver, url))
     try:
-        shadow_host1 = WebDriverWait(driver, 10).until(
+        shadow_host1 = WebDriverWait(driver, TIMEOUT).until(
             EC.presence_of_element_located(
-                (By.XPATH, "//*[starts-with(name(), 'macroponent')]")
+                (By.TAG_NAME, "macroponent-f51912f4c700201072b211d4d8c26010")
             )
         )
-        shadow_root1 = shadow_host1.shadow_root
-        iframe = WebDriverWait(shadow_root1, 10).until(
+        print(shadow_host1.shadow_root)
+        iframe = WebDriverWait(shadow_host1.shadow_root, TIMEOUT).until(
             EC.presence_of_element_located((By.CSS_SELECTOR, "iframe"))
         )
-        # shadow_root1.find_element(By.CSS_SELECTOR, "iframe")
         driver.switch_to.frame(iframe)
-        # dump_elements(driver.find_elements(By.CSS_SELECTOR, "#task_table"))
-        # print(driver.find_element(By.CSS_SELECTOR, "#task_table"))
-        table = WebDriverWait(driver, 10).until(
+        table = WebDriverWait(driver, TIMEOUT).until(
             EC.presence_of_element_located((By.CSS_SELECTOR, "table"))
         )
         print(parse_table(driver))
 
+    except TimeoutException as e:
+        print(f"Timed out while waiting for element: {e}")
+    except NoSuchShadowRootException as e:
+        print(f"Cannot find shadow root: {e}")
     except Exception as e:
+        print(type(e))
         print(f"An error occurred: {e}")
     finally:
         driver.quit()
